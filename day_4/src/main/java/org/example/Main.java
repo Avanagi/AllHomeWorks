7027ac26144a6e7c7f6feeb7ae105c36aa9ba84a
Roman <avanagi.payment@gmail.com>
2023-03-02 14:49:50 +0300

    Complete The Task

--- day_4/src/main/java/org/example/Main.java
+++ day_4/src/main/java/org/example/Main.java
@@ -3,25 +3,23 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        String[] words = {"like", "place"};
+        String[] words = {"like", "place", "first"};
         String[] resources = {
-                "http://www.gutenberg.org/cache/epub/17168/pg17168.txt",
-                "http://www.gutenberg.org/cache/epub/17168/pg17168.txt",
-                "http://www.gutenberg.org/cache/epub/17168/pg17168.txt",
-                "http://www.gutenberg.org/cache/epub/17168/pg17168.txt",
-                "http://www.gutenberg.org/cache/epub/17168/pg17168.txt",
-                "http://www.gutenberg.org/cache/epub/17168/pg17168.txt",
-                "http://www.gutenberg.org/cache/epub/17168/pg17168.txt",
-                "http://www.gutenberg.org/cache/epub/17168/pg17168.txt",
-                "http://www.gutenberg.org/cache/epub/17168/pg17168.txt",
-                "http://www.gutenberg.org/cache/epub/17168/pg17168.txt",
+                "./test/lp.txt",
+                "./test/lp.txt",
+                "./test/lp.txt",
+                "./test/lp.txt",
+                "./test/lp.txt",
+                "./test/lp.txt",
+                "./test/lp.txt",
+                "./test/lp.txt",
+                "./test/lp.txt",
                 "http://www.gutenberg.org/cache/epub/17168/pg17168.txt"};
         Occurrences occurrences = new OccurrencesImplement();
         occurrences.getOccurrences(resources, words, "./test/test.txt");
 
         long finish = System.currentTimeMillis();
         long elapsed = finish - start;
-
         System.out.println("Time gone, ms: " + elapsed);
     }
 }
--- day_4/src/main/java/org/example/MyThread.java
+++ day_4/src/main/java/org/example/MyThread.java
@@ -1,72 +1,64 @@
 package org.example;
 
-import org.jsoup.Jsoup;
-
 import java.io.*;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.ConcurrentSkipListSet;
 
 public class MyThread extends Thread {
-
     private final String fileDest;
     private final String[] words;
-    public ConcurrentSkipListSet<String> concurrentSkipListSetAllThread;
-    private final long skip;
+    private final ConcurrentSkipListSet<String> concurrentSkipListSetAllThread;
+    private final String res;
 
-    public MyThread(String fileDest, String[] words, ConcurrentSkipListSet<String> concurrentSkipListSetAllThread) {
+    public MyThread(String fileDest, String[] words, ConcurrentSkipListSet<String> concurrentSkipListSetAllThread, String res) {
         this.fileDest = fileDest;
         this.words = words;
         this.concurrentSkipListSetAllThread = concurrentSkipListSetAllThread;
-        this.skip = 0;
-    }
-
-    public MyThread(String fileDest, String[] words, ConcurrentSkipListSet<String> concurrentSkipListSetAllThread, long skip) {
-        this.fileDest = fileDest;
-        this.words = words;
-        this.concurrentSkipListSetAllThread = concurrentSkipListSetAllThread;
-        this.skip = skip;
+        this.res = res;
     }
 
     @Override
     public void run() {
         StringBuilder buffer = new StringBuilder();
-        InputStream is;
-        try {
-            is = new URL(fileDest).openStream();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        try (InputStreamReader inputStreamReader = new InputStreamReader(is, "UTF-16")){
+        try (InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(fileDest), StandardCharsets.UTF_8)) {
             BufferedReader reader = new BufferedReader(inputStreamReader);
-            reader.skip(skip);
             int ch;
-            while ((ch = reader.read())>-1){
-                if (((char)ch != '.')){
-                    buffer.append((char)ch);
-                }
-                else{
-                    String s = buffer.toString().trim();
-                    System.out.println(s);
-                    String[] split = s.split("\\b+");
-                    List<String> list = Arrays.asList(split);
-                    for (String s1 : words){
-                        if (list.contains(s1)) {
+            while ((ch = reader.read()) > - 1) {
+                if(((char) ch != '.') && ((char) ch != '?') && ((char) ch != '!')) {
+                    buffer.append((char) ch);
+                } else {
+                    for(String word: words) {
+                        if(checkWord(String.valueOf(buffer), word)) {
+                            String s = buffer.toString().trim();
                             concurrentSkipListSetAllThread.add(s);
                         }
                     }
                     buffer.setLength(0);
                 }
-                System.out.println(concurrentSkipListSetAllThread.toString());
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+        try {
+            BufferedWriter writer = Files.newBufferedWriter(Paths.get(res));
+            writer.write(String.valueOf(concurrentSkipListSetAllThread));
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public boolean checkWord(String line, String words) {
+        String[] strings = line.split(" ");
+        for(String word: strings) {
+            if(word.matches(words)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
