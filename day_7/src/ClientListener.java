c33872f539b3e5c02248d51834d68673c8e58525
Roman <avanagi.payment@gmail.com>
2023-03-29 11:04:51 +0300

    Task Complete!

--- day_7/src/ClientListener.java
+++ day_7/src/ClientListener.java
@@ -5,7 +5,7 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client {
+public class ClientListener {
 
     public static void main(String[] args) {
         System.out.print("Name - ");
